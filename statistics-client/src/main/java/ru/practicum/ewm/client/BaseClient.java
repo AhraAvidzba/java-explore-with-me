c0a2bb81b92d3660fa59d7bded5f5aaf09d3ef47
AkhraAvidzba <akhraa1@yandex.ru>
2023-08-02 19:35:27 +0300

    fix: добавить недостающие зависимости

--- statistics-client/src/main/java/ru/practicum/ewm/client/BaseClient.java
+++ statistics-client/src/main/java/ru/practicum/ewm/client/BaseClient.java
@@ -1,39 +1,42 @@
 package ru.practicum.ewm.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import ru.practicum.ewm.dtos.StatisticOutDto;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class BaseClient {
     protected final RestTemplate rest;
-
     public BaseClient(RestTemplate rest) {
         this.rest = rest;
     }
 
-    protected ResponseEntity<Object> get(String path, @Nullable Map<String, Object> parameters) {
+    protected ResponseEntity<String> get(String path, @Nullable Map<String, Object> parameters) {
         return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
     }
-    protected <T> ResponseEntity<Object> post(String path, T body) {
+    protected <T> ResponseEntity<String> post(String path, T body) {
         return makeAndSendRequest(HttpMethod.POST, path, null, body);
     }
 
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
+    private <T> ResponseEntity<String> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
 
-        ResponseEntity<Object> statisticServerResponse;
+        ResponseEntity<String> statisticServerResponse;
         try {
             if (parameters != null) {
-                statisticServerResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
+                statisticServerResponse = rest.exchange(path, method, requestEntity, String.class, parameters);
             } else {
-                statisticServerResponse = rest.exchange(path, method, requestEntity, Object.class);
+                statisticServerResponse = rest.exchange(path, method, requestEntity, String.class);
             }
         } catch (HttpStatusCodeException e) {
-            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
+            return ResponseEntity.status(e.getStatusCode()).body("");
         }
         return prepareGatewayResponse(statisticServerResponse);
     }
@@ -45,7 +48,7 @@ public class BaseClient {
         return headers;
     }
 
-    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
+    private static ResponseEntity<String> prepareGatewayResponse(ResponseEntity<String> response) {
         if (response.getStatusCode().is2xxSuccessful()) {
             return response;
         }
---
+++ statistics-client/src/main/java/ru/practicum/ewm/statistic/StatisticController.java
@@ -0,0 +1,40 @@
+package ru.practicum.ewm.statistic;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.dtos.StatisticInDto;
+import ru.practicum.ewm.dtos.StatisticOutDto;
+import ru.practicum.ewm.dtos.StatisticWithHitsDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class StatisticController {
+    private final StatisticClient statisticClient;
+
+    @PostMapping("/hit")
+    public StatisticOutDto addStatistic(@RequestBody StatisticInDto statisticInDto) {
+        log.info("adding statistic");
+        return statisticClient.addStatistic(statisticInDto);
+    }
+
+    @GetMapping("/stats")
+    public List<StatisticWithHitsDto> getStatistics(@RequestParam(name = "start") String strStart,
+                                                    @RequestParam(name = "end") String strEnd,
+                                                    @RequestParam(defaultValue = "") String[] uris,
+                                                    @RequestParam(defaultValue = "false") boolean unique) {
+        log.info("Getting statistic");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime start = LocalDateTime.parse(strStart, formatter);
+        LocalDateTime end = LocalDateTime.parse(strEnd, formatter);
+
+        strStart = start.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        strEnd = end.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        return statisticClient.getStatistics(strStart, strEnd, uris, unique);
+    }
+}
