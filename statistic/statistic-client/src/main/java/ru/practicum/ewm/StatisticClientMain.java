94ee58764eb663776bb131f36910fc896205b9f8
AkhraAvidzba <akhraa1@yandex.ru>
2023-08-03 00:27:31 +0300

    feat: добавить докерфайл и докеркомпоуз

--- statistic/statistic-client/src/main/java/ru/practicum/ewm/StatisticClientMain.java
+++ statistic/statistic-client/src/main/java/ru/practicum/ewm/StatisticClientMain.java
@@ -1,12 +1,12 @@
-package ru.practicum.ewm;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class StatisticClientMain {
-    public static void main(String[] args) {
-        SpringApplication.run(StatisticClientMain.class, args);
-    }
-}
+//package ru.practicum.ewm;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class StatisticClientMain {
+//    public static void main(String[] args) {
+//        SpringApplication.run(StatisticClientMain.class, args);
+//    }
+//}
