043d85ef2def878b6ddcee35c800f35fb03024c3
AkhraAvidzba <akhraa1@yandex.ru>
2023-08-17 16:24:14 +0300

    style: откорректировать стиль где необходимо

--- ewm/src/main/java/ru/practicum/ewm/request/RequestServiceImpl.java
+++ ewm/src/main/java/ru/practicum/ewm/request/RequestServiceImpl.java
@@ -42,9 +42,7 @@ public class RequestServiceImpl implements RequestService {
                 .orElseThrow(() -> new ContentNotFoundException("Событие не найдено"));
         requestRepository.findRequestByRequesterIdAndEventId(userId, eventId)
                 .ifPresent((x) ->
-                {
-                    throw new ContentAlreadyExistException("Запрос на участие в данном событии был ранее уже отправлен пользователем");
-                });
+                {throw new ContentAlreadyExistException("Запрос на участие в данном событии был ранее уже отправлен пользователем");});
         if (user.getId().equals(event.getInitiator().getId())) {
             throw new ContentAlreadyExistException("Инициатор события не может добавить запрос на участие в своём событии");
         }
